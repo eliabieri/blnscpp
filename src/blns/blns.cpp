29efbe220cb190b209b42578a4bf3780009e4a6a
teskann <teskann.dev@gmail.com>
2022-05-30 01:19:27 +0200

    Remove comments from blns txt file

--- src/blns/blns.cpp
+++ src/blns/blns.cpp
@@ -1,5 +1,7 @@
 #include <blns/blns.h>
 #include <cassert>
+#include <algorithm>
+#include <string>
 
 blns::Blns::Blns() {
         std::ifstream blnsFile = std::filesystem::path(__FILE__).parent_path()
@@ -8,9 +10,13 @@ blns::Blns::Blns() {
         // If this assertion fails, ensure that you have correctly initialized and updated the submodules for this repo
         assert(blnsFile.is_open());
 
-        std::copy(std::istream_iterator<std::string>(blnsFile),
-                  std::istream_iterator<std::string>(),
-                  std::back_inserter(blns));
+        std::string line;
+        while(getline(blnsFile, line)) {
+            // Skip comments in txt file
+            if(line.rfind('#', 0) != 0) {
+                blns.push_back(line);
+            }
+        }
         assert(!blns.empty());
 }
 
